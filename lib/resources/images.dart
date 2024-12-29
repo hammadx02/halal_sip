ce8844369699cb8461b0a5edb3b9f75e834d0eb1
Hammad Ali <hammadaali02@gmail.com>
2024-12-29 21:43:25 +0500

    Add new mojito icon to Images resource

--- lib/resources/images.dart
+++ lib/resources/images.dart
@@ -4,8 +4,8 @@ abstract class Images {
   static const kIconShare = base + "ic_share.png";
   static const kIconMenu = base + "ic_menu.png";
   static const kIconBottle = base + "ic_beer_bottel.png";
+  static const kIconBottle2 = base + "mojito.png";
   static const kIconDetail = base + "ic_detail.png";
   static const kIconDots = base + "ic_dots.png";
   static const kIconRight = base + "ic_right.png";
-
 }
