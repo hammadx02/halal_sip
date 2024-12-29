37442d2f352515cf5ed2969c6b3c821c1373b77b
Hammad Ali <hammadaali02@gmail.com>
2024-12-29 21:35:25 +0500

    Refactor DetailPage to include index parameter and update widget tags for uniqueness

--- lib/home.dart
+++ lib/home.dart
@@ -1,9 +1,167 @@
+// import 'package:flutter/material.dart';
+// import 'package:halal_sip/detail.dart';
+// import 'package:halal_sip/resources/Fonts.dart';
+// import 'package:halal_sip/resources/colors.dart';
+// import 'package:halal_sip/resources/images.dart';
+
+// class Home extends StatelessWidget {
+//   const Home({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: buildAppBar(),
+//       body: SingleChildScrollView(
+//         // Wrap the content with SingleChildScrollView
+//         padding: const EdgeInsets.only(left: 40.0),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             const SizedBox(height: 20.0),
+//             RichText(
+//               text: const TextSpan(
+//                   text: "Halal Drinks\nAwards",
+//                   style: textStyle34px700w,
+//                   children: [
+//                     WidgetSpan(child: SizedBox(width: 30.0)),
+//                     TextSpan(
+//                       text: "Mocktails",
+//                       style: textStyleOrange34px700w,
+//                     )
+//                   ]),
+//             ),
+//             const SizedBox(height: 50.0),
+//             _buildItem(context, "01", "Lemon Mojito", "Morocco",
+//                 Images.kIconBottle, AppColors.orange.withOpacity(.7)),
+//             const SizedBox(height: 50.0),
+//             _buildItem(context, "02", "Strawberry Fizz", "Lebanon",
+//                 Images.kIconBottle, Colors.green.withOpacity(.7)),
+//             const SizedBox(height: 50.0),
+//             _buildItem(context, "03", "Virgin Pina Colada", "Indonesia",
+//                 Images.kIconBottle, Colors.blue.withOpacity(.7)),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   Widget _buildItem(BuildContext context, String number, String title,
+//       String subtitle, String image, Color color) {
+//     return Stack(
+//       alignment: Alignment.center,
+//       children: [
+//         Container(
+//           height: 180.0,
+//           decoration: BoxDecoration(
+//             color: color, // Update the color according to the drink
+//             borderRadius: const BorderRadius.only(
+//               topLeft: Radius.circular(30.0),
+//               bottomLeft: Radius.circular(30.0),
+//             ),
+//           ),
+//         ),
+//         Align(
+//           alignment: Alignment.centerLeft,
+//           child: Row(
+//             children: [
+//               const SizedBox(width: 30.0),
+//               Container(
+//                 decoration: BoxDecoration(
+//                   boxShadow: [
+//                     BoxShadow(
+//                         color: color.withOpacity(0.1),
+//                         offset: const Offset(10, 10),
+//                         blurRadius: 10,
+//                         spreadRadius: 5)
+//                   ],
+//                 ),
+//                 child: Hero(
+//                   tag: "bottle",
+//                   child: Image.asset(image, height: 290),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//         Positioned(
+//           top: 30,
+//           right: 40.0,
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               Container(
+//                 height: 55.0,
+//                 width: 50.0,
+//                 decoration: const BoxDecoration(
+//                   color: AppColors.orange,
+//                   borderRadius: BorderRadius.all(Radius.circular(10.0)),
+//                 ),
+//                 child:
+//                     Center(child: Text(number, style: textStyleWhite18px600w)),
+//               ),
+//               const SizedBox(height: 30.0),
+//               RichText(
+//                 text: TextSpan(
+//                     text: "$title\n",
+//                     style: textStyleDarkHeavy24px700,
+//                     children: [
+//                       WidgetSpan(child: SizedBox(height: 34)),
+//                       TextSpan(
+//                         text: subtitle,
+//                         style: textStyleSubText18px700w,
+//                       )
+//                     ]),
+//               ),
+//             ],
+//           ),
+//         ),
+//         Positioned(
+//           bottom: 20,
+//           right: 30.0,
+//           child: InkWell(
+//             onTap: () {
+//               Navigator.push(context,
+//                   MaterialPageRoute(builder: (context) => DetailPage()));
+//             },
+//             child: Container(
+//               height: 65.0,
+//               width: 65.0,
+//               decoration: const BoxDecoration(
+//                 color: AppColors.colorPrimary,
+//                 borderRadius: BorderRadius.all(Radius.circular(20.0)),
+//               ),
+//               child: Padding(
+//                 padding: const EdgeInsets.all(20.0),
+//                 child: Image.asset(Images.kIconDetail),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+
+//   AppBar buildAppBar() {
+//     return AppBar(
+//       shadowColor: Colors.transparent,
+//       actions: [
+//         const SizedBox(width: 40.0),
+//         Image.asset(Images.kIconTrophy, height: 24.0, width: 24),
+//         const Spacer(),
+//         Image.asset(Images.kIconShare, height: 18.0, width: 18),
+//         const SizedBox(width: 50.0),
+//         Image.asset(Images.kIconMenu, height: 18.0, width: 24),
+//         const SizedBox(width: 30.0),
+//       ],
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:halal_sip/detail.dart';
+import 'package:halal_sip/resources/Fonts.dart';
 import 'package:halal_sip/resources/colors.dart';
 import 'package:halal_sip/resources/images.dart';
-
-import 'resources/Fonts.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -13,48 +171,49 @@ class Home extends StatelessWidget {
     return Scaffold(
       appBar: buildAppBar(),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 40.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 20.0),
-              RichText(
-                text: const TextSpan(
-                    text: "Craft Craft\nBeer\nAwards",
-                    style: textStyle34px700w,
-                    children: [
-                      WidgetSpan(child: SizedBox(width: 30.0)),
-                      TextSpan(
-                        text: "IPA",
-                        style: textStyleOrange34px700w,
-                      )
-                    ]),
-              ),
-              const SizedBox(height: 50.0),
-              _buildItem(
-                  context, "01", "Purple Rain", "France", Images.kIconBottle),
-              const SizedBox(height: 50.0),
-              _buildItem(context, "02", "La Dodo", "", Images.kIconBottle),
-              const SizedBox(height: 50.0),
-              _buildItem(context, "03", "Gallia", "", Images.kIconBottle),
-            ],
-          ),
+        // Wrap the content with SingleChildScrollView
+        padding: const EdgeInsets.only(left: 40.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 20.0),
+            RichText(
+              text: const TextSpan(
+                  text: "Halal Drinks\nAwards",
+                  style: textStyle34px700w,
+                  children: [
+                    WidgetSpan(child: SizedBox(width: 30.0)),
+                    TextSpan(
+                      text: "Mocktails",
+                      style: textStyleOrange34px700w,
+                    )
+                  ]),
+            ),
+            const SizedBox(height: 50.0),
+            _buildItem(context, "01", "Lemon Mojito", "Morocco",
+                Images.kIconBottle, AppColors.orange.withOpacity(.7), 1),
+            const SizedBox(height: 50.0),
+            _buildItem(context, "02", "Strawberry Fizz", "Lebanon",
+                Images.kIconBottle, Colors.green.withOpacity(.7), 2),
+            const SizedBox(height: 50.0),
+            _buildItem(context, "03", "Virgin Pina Colada", "Indonesia",
+                Images.kIconBottle, Colors.blue.withOpacity(.7), 3),
+          ],
         ),
       ),
     );
   }
 
   Widget _buildItem(BuildContext context, String number, String title,
-      String subtitle, String image) {
+      String subtitle, String image, Color color, int index) {
     return Stack(
       alignment: Alignment.center,
       children: [
         Container(
           height: 180.0,
-          decoration: const BoxDecoration(
-            color: AppColors.colorSecondary,
-            borderRadius: BorderRadius.only(
+          decoration: BoxDecoration(
+            color: color, // Update the color according to the drink
+            borderRadius: const BorderRadius.only(
               topLeft: Radius.circular(30.0),
               bottomLeft: Radius.circular(30.0),
             ),
@@ -65,19 +224,23 @@ class Home extends StatelessWidget {
           child: Row(
             children: [
               const SizedBox(width: 30.0),
-              Container(
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                        color: AppColors.colorPrimary.withOpacity(0.1),
-                        offset: const Offset(10, 10),
-                        blurRadius: 10,
-                        spreadRadius: 5)
-                  ],
-                ),
-                child: Hero(
-                  tag: "bottle",
-                  child: Image.asset(image, height: 290),
+              AnimatedOpacity(
+                opacity: 1.0,
+                duration: Duration(milliseconds: 500),
+                child: Container(
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          color: color.withOpacity(0.1),
+                          offset: const Offset(10, 10),
+                          blurRadius: 10,
+                          spreadRadius: 5)
+                    ],
+                  ),
+                  child: Hero(
+                    tag: "bottle$index", // Unique tag for each Hero widget
+                    child: Image.asset(image, height: 290),
+                  ),
                 ),
               ),
             ],
@@ -120,19 +283,28 @@ class Home extends StatelessWidget {
           right: 30.0,
           child: InkWell(
             onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => DetailPage()));
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailPage(
+                    index: index,
+                  ),
+                ),
+              );
             },
-            child: Container(
-              height: 65.0,
-              width: 65.0,
-              decoration: const BoxDecoration(
-                color: AppColors.colorPrimary,
-                borderRadius: BorderRadius.all(Radius.circular(20.0)),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Image.asset(Images.kIconDetail),
+            child: ScaleTransition(
+              scale: AlwaysStoppedAnimation(1.0),
+              child: Container(
+                height: 65.0,
+                width: 65.0,
+                decoration: const BoxDecoration(
+                  color: AppColors.colorPrimary,
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Image.asset(Images.kIconDetail),
+                ),
               ),
             ),
           ),
@@ -144,8 +316,6 @@ class Home extends StatelessWidget {
   AppBar buildAppBar() {
     return AppBar(
       shadowColor: Colors.transparent,
-      elevation: 0,
-      backgroundColor: Colors.white,
       actions: [
         const SizedBox(width: 40.0),
         Image.asset(Images.kIconTrophy, height: 24.0, width: 24),
